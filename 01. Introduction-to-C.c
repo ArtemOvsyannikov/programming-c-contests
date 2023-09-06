7ddd2e5e6d7b686ee9c877b4fbf9ee45a8fb4bc3
sh1ron <artemrotfront@gmail.com>
2023-09-06 08:04:56 +0300

    first contest - update and refactoring

--- 01. Introduction-to-C.c
+++ 01. Introduction-to-C.c
@@ -23,16 +23,23 @@ TASK 2. The sum of the digits of a five-digit number
 
 #include <stdio.h>
 
-int main()
-{
-    int input_number = 0, sum_of_digits = 0;
+int main() {
+    int input_number = 0;
     scanf("%d", &input_number);
-    for (int i = 0; i < 5; i++)
-    {
-        sum_of_digits += input_number % 10;
-        input_number /= 10;
-    }
-    printf("%d", sum_of_digits);
+
+    int digit_sum = 0;
+    digit_sum += input_number % 10;
+    input_number /= 10;
+    digit_sum += input_number % 10;
+    input_number /= 10;
+    digit_sum += input_number % 10;
+    input_number /= 10;
+    digit_sum += input_number % 10;
+    input_number /= 10;
+    digit_sum += input_number;
+
+    printf("%d\n", digit_sum);
+
     return 0;
 }
 
@@ -41,17 +48,26 @@ TASK 3. An inverted five-digit number
 */
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    int N;
-    scanf("%d", &N);
-    int digits[5] = {0};
-    int i = 0;
-    while (N > 0) {
-        digits[i++] = N % 10;
-        N /= 10;
-    }
-    printf("%d\n", digits[0] * 10000 + digits[1] * 1000 + digits[2] * 100 + digits[3] * 10 + digits[4]);
+    int input_number;
+    scanf("%d", &input_number);
+
+    int second_digit, third_digit, fourth_digit, fifth_digit;
+    fifth_digit = input_number % 10;
+    input_number/=10;
+    fourth_digit = input_number % 10;
+    input_number/=10;
+    third_digit = input_number % 10;
+    input_number/=10;
+    second_digit = input_number % 10;
+    input_number/=10;
+
+    printf("%d\n", fifth_digit * 10000 + fourth_digit * 1000 + 
+            third_digit * 100 + second_digit * 10 + input_number);
+
+    return 0;
 }
 
 /*
@@ -62,9 +78,12 @@ TASK 4. Time in seconds
 
 int main() {
     int hours = 0, minutes = 0, seconds = 0;
-    scanf("%d%d%d", &hours, &minutes, &seconds);
+    scanf("%d %d %d", &hours, &minutes, &seconds);
+
     int totalSeconds = hours * 3600 + minutes * 60 + seconds;
+
     printf("%d", totalSeconds);
+    
     return 0;
 }
 
@@ -77,39 +96,12 @@ TASK 5. Time in format
 int main() {
     int totalSeconds = 0;
     scanf("%d", &totalSeconds); 
+
     int hours = totalSeconds / 3600;
     int minutes = (totalSeconds / 60) % 60; 
     int seconds = totalSeconds % 60; 
+
     printf("%02d:%02d:%02d", hours, minutes, seconds); 
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
